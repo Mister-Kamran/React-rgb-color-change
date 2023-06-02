32ac477095ca7963567d5ae14346a840287bc8a9
Mister-Kamran <135120735+Mister-Kamran@users.noreply.github.com>
2023-06-02 13:04:05 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState,useEffect  } from 'react';
+
+function Test() {
+  const color_bg = useRef(null);
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+  const [bg_color] = useState(['red', 'green', 'blue']); 
+
+
+  useEffect(() => {
+
+    color_bg.current.style.backgroundColor = bg_color[currentColorIndex]; 
+
+  },);
+
+
+  const handleClick = () => {
+    
+    color_bg.current.style.backgroundColor = bg_color[currentColorIndex]; 
+
+    setCurrentColorIndex((currentColorIndex + 1));
+
+    console.log(currentColorIndex)
+
+
+  if(currentColorIndex===2){
+    setCurrentColorIndex(0);
+  }
+     
+  };
+
+  return (
+    <div>
+      <div className='game_container'>
+        <button ref={color_bg} onClick={handleClick}>Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
